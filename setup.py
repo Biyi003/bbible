0b084eb294245f09514f7ed728ca0cf545c66c47
Biyi Adebayo <adebiyi87@gmail.com>
2025-07-03 19:46:07 +0100

    v0.2.1: updated description for improved usability

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bbible",
-    version="0.2.0",
+    version="0.2.1",
     author="Biyi Adebayo",
     description="Explore the Bible by verse or by meaning — with multi-version and AI topic search",
     long_description=open("README.md", encoding="utf-8").read(),
